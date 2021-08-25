404aa7d0a51822cf527e9bfd2586fe798f625f05
bhagyawijenayake <bhagyasudaraka98@gmail.com>
2021-08-25 10:59:52 +0530

    v1 and v2 create

---
+++ src/java/com/kelaniya/uni/v1/main.java
@@ -0,0 +1,10 @@
+package com.kelaniya.uni.v1;
+
+public class main {
+
+    public static void main(String[] ags){
+
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ src/java/com/kelaniya/uni/v2/main.java
@@ -0,0 +1,10 @@
+package com.kelaniya.uni.v2;
+
+public class main {
+
+    public static void main(String[] ags){
+
+        System.out.println("Hello World!");
+    }
+
+}
