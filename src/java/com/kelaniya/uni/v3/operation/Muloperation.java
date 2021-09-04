d41ad1c8907e1a69d465c15414af608bba712ad2
bhagyawijenayake <bhagyasudaraka98@gmail.com>
2021-09-04 20:00:05 +0530

    v3 final code

---
+++ src/java/com/kelaniya/uni/v3/operation/Muloperation.java
@@ -0,0 +1,9 @@
+package com.kelaniya.uni.v3.operation;
+
+public class Muloperation {
+    public Double execute(Double[] numbers){
+
+        return numbers[0] * numbers[1];
+
+    }
+}
---
+++ src/java/com/kelaniya/uni/v3/operation/Suboperation.java
@@ -0,0 +1,9 @@
+package com.kelaniya.uni.v3.operation;
+
+public class Suboperation {
+    public Double execute(Double[] numbers){
+
+        return numbers[0] - numbers[1];
+
+    }
+}
